a271a56a53c4ebfcb90e8f5cf799c617ded4a756
Jeremy ho <eneka.ken@gmail.com>
2020-03-25 16:27:46 -0700

    progress on queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,20 +2,29 @@ var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
-  storage.count = 0
+  someInstance.storage = {};
+  someInstance.count = 0
+  //{count:0,
+  //0: value}
 
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+
     //add a string to back of queue
+    if (someInstance.count === 0) {
+      someInstance.storage[someInstance.count] = value;
+      //console.log(storage[someInstance.count])
+      someInstance.count ++;
+    }
+
   };
 
   someInstance.dequeue = function() {
+    someInstance.count --
   };
 
   someInstance.size = function() {
-    return storage.count
+    return someInstance.count
   };
 
   return someInstance;
