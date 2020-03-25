1ee21106e4f92b14dbc2f0fca38726e231ec05e6
Jeremy ho <eneka.ken@gmail.com>
2020-03-25 15:28:32 -0700

    completed stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,19 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  storage.count = 0
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    //add a string to back of queue
   };
 
   someInstance.dequeue = function() {
   };
 
   someInstance.size = function() {
+    return storage.count
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,29 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  storage.count = 0
 
   // Implement the methods below
   someInstance.push = function(value) {
+   storage[storage.count] = value
+   storage.count ++
+
   };
 
   someInstance.pop = function() {
+    storage.count --
+   if (storage.count < 0){
+    storage.count = 0
+   }
+return storage[storage.count]
+
+
   };
 
   someInstance.size = function() {
+    return storage.count
   };
 
   return someInstance;
 };
+
