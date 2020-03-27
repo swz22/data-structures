94ac6c9aa145ab30ade6266f6c1c5128e8a75c9c
Jeremy ho <eneka.ken@gmail.com>
2020-03-26 20:39:53 -0700

    completed es6

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,35 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
   constructor() {
+    this.storage ={};
+    this.count = 0 ;
   }
+
+  enqueue(value) {
+  this.storage[this.count] = value;
+  this.count++;
+}
+
+  dequeue() {
+  if (this.count >= 1) {
+    for (var key in this.storage){
+      this.storage[key - 1] = this.storage[key];
+    }
+     delete this.storage[this.count];
+     this.count --;
+    return this.storage['-1'];
+  } else {
+      delete this.storage['0'];
+      return this.storage['0'];
+    }
+ if (this.count < 0) {
+  this.count = 0;
+    };
+  }
+
+  size() {
+  return this.count;
+  }
+
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,26 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
   }
 
-}
+
+push(value){
+  this.storage[this.count] = value;
+  this.count++;
+};
+
+pop(){
+  this.count --;
+  if (this.count < 0){
+    this.count = 0;
+  }
+  return this.storage[this.count]
+};
+
+size(){
+  return this.count;
+};
+
+};
+var stackmethods = new Stack()
