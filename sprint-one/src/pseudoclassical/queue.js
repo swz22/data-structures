af419722285d91f62fc71e1ecb19afbd29a7180b
Jeremy ho <eneka.ken@gmail.com>
2020-03-26 20:01:59 -0700

    completed pseudo Queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,33 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+ this.storage = {};
+ this.count = 0
 };
 
+Queue.prototype.enqueue = function(value){
+  this.storage[this.count] = value;
+  this.count++;
+};
+
+Queue.prototype.dequeue = function(){
+  if (this.count >= 1){
+    for (var key in this.storage){
+      this.storage[key - 1] = this.storage[key];
+    }
+     delete this.storage[this.count];
+     this.count --;
+    return this.storage['-1'];
+  } else {
+      delete this.storage['0'];
+      return this.storage['0'];
+    }
+ if (this.count < 0){
+  this.count = 0;
+    };
+};
+
+Queue.prototype.size = function(){
+  return this.count;
+};
+
+var queueMethos = new Queue
 
