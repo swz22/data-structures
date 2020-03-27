cd7084e820e0c8567027441bb47997989ce9d498
Jeremy ho <eneka.ken@gmail.com>
2020-03-26 19:59:21 -0700

    pseudo stack.js

---
+++ sprint-one/src/pseudoclassical/stack.js
@@ -0,0 +1,24 @@
+var Stack = function (){
+
+this.storage = {};
+this.count = 0
+};
+
+Stack.prototype.push = function(value){
+  this.storage[this.count] = value;
+  this.count++;
+}
+
+Stack.prototype.pop = function(){
+    this.count --;
+    if (this.count < 0){
+      this.count = 0
+    }
+    return this.storage[this.count]
+  };
+
+  Stack.prototype.size = function(){
+    return this.count;
+  };
+
+  var stackmethods = new Stack()
