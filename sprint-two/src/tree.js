52d4ee528bcfcf7d156fb93acbb1699baa705fe0
Jeremy ho <eneka.ken@gmail.com>
2020-03-27 19:16:26 -0700

    completed tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,8 @@
 var Tree = function(value) {
   var newTree = {};
+  newTree.children = [];
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,13 +10,39 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var childTree = Tree(value);
+  this.children.push(childTree);
 };
+
 
 treeMethods.contains = function(target) {
-};
+  if (this.value === target) {
+    return true;
+  }
 
+  if (this.children.length) {
+    for (let i = 0; i < this.children.length; i++) {
+
+      let childsIdentity = this.children[i];
+        if (childsIdentity.contains(target)) {
+          return true;
+      }
+    }
+
+    return false;
+  }
+};
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+
+//IMPLEMENT
+// A tree class, in functional with shared methods style, with the following properties:
+//  .children property, an array containing a number of subtrees
+//  .addChild() method, takes any value, sets that as the target of a node, and adds that node as a child of the tree
+//  A .contains() method, takes any input and returns a boolean reflecting whether it can be found as the value of the target node or any descendant node
+//  What is the time complexity of the above functions?
